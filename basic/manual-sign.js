76d29dc7caefdc9e8c3837c7a60d31b9cdfceb86
Pathorn Teng <pathorn.teng@gmail.com>
2025-09-22 12:06:33 +0700

    format the code

--- basic/manual-sign.js
+++ basic/manual-sign.js
@@ -1,41 +1,41 @@
 const {
-  Client,
-  AccountId,
-  PrivateKey,
-  TransferTransaction,
-  Hbar,
-  Transaction,
-  TransactionId,
+    Client,
+    AccountId,
+    PrivateKey,
+    TransferTransaction,
+    Hbar,
+    Transaction,
+    TransactionId,
 } = require("@hashgraph/sdk");
 require("dotenv").config();
 
 async function main() {
-  const client = Client.forTestnet();
+    const client = Client.forTestnet();
 
-  const myAccountId = process.env.MY_ACCOUNT_ID;
-  const myPrivateKey = PrivateKey.fromString(process.env.MY_PRIVATE_KEY);
-  //client.setOperator(operatorId, operatorKey);
+    const myAccountId = process.env.MY_ACCOUNT_ID;
+    const myPrivateKey = PrivateKey.fromString(process.env.MY_PRIVATE_KEY);
+    //client.setOperator(operatorId, operatorKey);
 
-  let transaction = new TransferTransaction()
-    .addHbarTransfer(myAccountId, Hbar.fromTinybars(-100))
-    .addHbarTransfer(AccountId.fromString("0.0.3"), Hbar.fromTinybars(100))
-    .setNodeAccountIds([AccountId.fromString("0.0.5")])
-    .setTransactionId(TransactionId.generate(myAccountId))
-    .freezeWith(client);
+    let transaction = new TransferTransaction()
+        .addHbarTransfer(myAccountId, Hbar.fromTinybars(-100))
+        .addHbarTransfer(AccountId.fromString("0.0.3"), Hbar.fromTinybars(100))
+        .setNodeAccountIds([AccountId.fromString("0.0.5")])
+        .setTransactionId(TransactionId.generate(myAccountId))
+        .freezeWith(client);
 
-  // Get body bytes to sign
-  const bodyBytes = transaction._signedTransactions.get(0).bodyBytes
-  
-  // Sign with the private key
-  const signature = myPrivateKey.sign(bodyBytes);
+    // Get body bytes to sign 
+    const bodyBytes = transaction._signedTransactions.get(0).bodyBytes
 
-  transaction.addSignature(myPrivateKey.publicKey, signature);
+    // Sign with the private key
+    const signature = myPrivateKey.sign(bodyBytes);
 
-  const txResponse = await transaction.execute(client);
-  const receipt = await txResponse.getReceipt(client);
-  console.log("Transaction status: " + receipt.status.toString());
+    transaction.addSignature(myPrivateKey.publicKey, signature);
 
-  client.close();
+    const txResponse = await transaction.execute(client);
+    const receipt = await txResponse.getReceipt(client);
+    console.log("Transaction status: " + receipt.status.toString());
+
+    client.close();
 }
 
 void main();
